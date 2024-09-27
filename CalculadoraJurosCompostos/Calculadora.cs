5813c0f3083457806460af903cfb61a146f4dece
USUARIO <USUARIO@SUPORTE2-PC>
2024-09-27 14:25:29 -0300

    Add project files.

---
+++ CalculadoraJurosCompostos/Calculadora.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraJurosCompostos
+{
+    internal class Calculadora
+    {
+        private double Valor { get; set; }
+        private double TaxaJuros { get; set; }
+        private int Anos { get; set; }
+        private int Meses { get; set; }
+        public void SetMeses (int meses)
+        {
+            if (meses >= 0)
+            {
+                Meses = meses;
+            }
+            else
+            {
+                throw new ArgumentException("O valor não pode ser negativo!");
+            }
+        }
+        public void SetValor(double valor)
+        {
+            if (valor > 0)
+            {
+                Valor = valor;
+            } else
+            {
+                throw new ArgumentException("O valor do montante deve ser maior que 0!");
+            }
+        }
+        public void SetTaxaJuros (double taxajuros)
+        {
+            if (taxajuros > 0)
+            {
+                TaxaJuros = taxajuros;
+            }
+            else
+            {
+                throw new ArgumentException("O valor do juros deve ser maior que 0!");
+            }
+        }
+        public void SetTempo (int tempo)
+        {
+            if (tempo > 0)
+            {
+                Anos = tempo;
+            }
+            else
+            {
+                throw new ArgumentException("O valor de anos deve ser maior que 0");
+            }
+        }
+        public double CalcualarMontante ()
+        {
+            double i = TaxaJuros/100;  // Converte a taxa de juros para decimal
+            int n = 12;  // Número de períodos (mensal)
+            double t = Anos;
+
+            // Cálculo do montante para o valor inicial
+            double montanteInicial = Valor * Math.Pow((1 + i / n), n * t);
+
+            // Cálculo do montante para a contribuição mensal
+            double montanteContribuicoes = Meses * (Math.Pow((1 + i / n), n * t) - 1) / (i / n);
+
+            // Retorna o montante total
+            return montanteInicial + montanteContribuicoes;
+        }
+    }
+}
---
+++ CalculadoraJurosCompostos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraJurosCompostos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            Console.Write("Digite o valor investido (R$): ");
+            Double valor = Convert.ToDouble(Console.ReadLine());
+
+            calculadora.SetValor(valor);
+
+            Console.Write("Digite a taxa de juros (em %): ");
+            double taxajuros = Convert.ToDouble(Console.ReadLine());
+
+            calculadora.SetTaxaJuros(taxajuros);
+
+            Console.Write("Digite sua contribuição mensal (R$): ");
+            int meses = Convert.ToInt32(Console.ReadLine());
+
+            calculadora.SetMeses(meses);
+
+            Console.Write("Digite a quantia de tempo (em Anos): ");
+            int tempo = Convert.ToInt32(Console.ReadLine());
+
+            calculadora.SetTempo(tempo);
+
+            Console.WriteLine();
+
+            double montante = calculadora.CalcualarMontante();
+            Console.WriteLine($"O montante final após {tempo} anos é: {montante:C2}");
+
+            Console.ReadKey();
+        }
+    }
+}
